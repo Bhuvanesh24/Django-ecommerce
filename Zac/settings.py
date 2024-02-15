d36463855a64473db477a2bb704670cc76d38591
Bhuvanesh24 <132251713+Bhuvanesh24@users.noreply.github.com>
2024-02-15 14:49:40 +0530

    Update settings.py

--- Zac/settings.py
+++ Zac/settings.py
@@ -139,8 +139,8 @@ MESSAGE_TAGS ={
 }
 
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='bhuvaneshwaran813@gmail.com'
-EMAIL_HOST_PASSWORD='jkczmprjidobcrro'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
